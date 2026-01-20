3c6906ab0a59e79db4ec22e9e773c10f857b6f62
surajyaduvanshi07 <surajyaduvanshi0706@gmail.com>
2026-01-20 11:01:26 -0800

    day 1 start

--- Python-day1.py
+++ Python-day1.py
@@ -1,1 +1,18 @@
 print("Hello World")
+# import sys
+# print(sys.version)
+print("Hello");print("My name is Suraj yadav");print("bye bye!")#we can use semicolon to execute multiple statement in one line
+print("Hello World",end=" ")
+print("I will print on the same line.")
+print("I am",19,"years old.")#combine text and number in one output by separating comma','
+#VARIABLES
+x = 5
+y = "Suraj"
+print(x)
+print(y)
+#TYPE CASTING
+x = str(3)
+y = int(3)
+z = float(3)
+name = "suraj"
+print(type(x),type(y),type(z),type(name))
