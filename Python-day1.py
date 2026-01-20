7959be1596f368cffca8aa7775d5b831794a7536
surajyaduvanshi07 <surajyaduvanshi0706@gmail.com>
2026-01-20 12:38:58 -0800

    day 1 start

--- Python-day1.py
+++ Python-day1.py
@@ -16,3 +16,53 @@ y = int(3)
 z = float(3)
 name = "suraj"
 print(type(x),type(y),type(z),type(name))#we can use type() to print type of a variable
+#OPERATORS
+#1 ARITHMETICS OPERATOR
+a = 5
+b = 3
+sum = a + b
+print("Sum of a and b is : ",sum)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+#2 RELATIONAL OPERATOR
+a = 50 
+b = 20
+print(a == b)#False
+print(a > b)#True
+print(a != b)#True
+print(a >= b)#True
+print(a <= b)#False
+print(a < b)#False
+#3 ASSIGNMENT OPERATOR
+num = 10
+num += 10
+num **= 2
+# num -= 10
+# num *= 10
+# num /= 10
+print(num)
+#4 LOGICAL OPERATOR
+print(not(False))
+print(not(True))
+#AND OPERATOR
+val1 = True
+val2 = False
+print("and operator", val1 and val2)
+print("and operator", True and False)
+print("and operator", True and True)
+print("and operator", False and True)
+print("and operator", False and False)
+#or operator
+val1 = True
+val2 = False
+print("and operator", val1 or val2)
+print("and operator", True or False)
+print("and operator", True or True)
+print("and operator", False or True)
+print("and operator", False or False)
+
+
+
+
