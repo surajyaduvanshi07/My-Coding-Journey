38dea1669bfc1639fefc1cd6d192a3870d9b796f
surajyaduvanshi07 <surajyaduvanshi0706@gmail.com>
2026-01-21 11:48:58 -0800

    started

---
+++ practice-day2.py
@@ -0,0 +1,22 @@
+#WAP to input user's first name and print its length
+name = input("Enter your name :")
+print(len(name))
+#WAP to find the occurrence of "$" in a string
+a = "I am $$$ and my value is 1$"
+print(a.count("$"))
+##WAP that converts a string to uppercase and lower case
+a = "i am suraj"
+print(a.upper())
+b = "MY NAME IS SURAJ"
+print(b.lower())
+#WAP that return the first character of a string
+a = "suraj yadav"
+print(a[0])
+#WAP that checks if a substring exists inside a string
+a = "my name is suraj"
+print(a.endswith("aj"))
+print(a.replace(" ","_"))
+#WAP that join two string with a hyphen(-)
+str1 = "suraj"
+str2 = "yadav"
+print(str1+"-"+str2)
