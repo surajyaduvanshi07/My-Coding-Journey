3e511bef2530fa7c81bd592a72ed060e01b5615d
surajyaduvanshi07 <surajyaduvanshi0706@gmail.com>
2026-01-22 10:43:49 -0800

    One task done

--- Python-day3.py
+++ Python-day3.py
@@ -1,30 +1,47 @@
-# #LIST IN PYTHON (built in data type)
-# marks = [34.4,52.4,64.7,99.4]
-# print(marks,type(marks))
-# #INDEXING
-# print(marks[2])
-# #LENGTH OF LIST
-# print(len(marks))
-# #Store elements of different types
-# student = ["Suraj",91.2,19,"Rohtak"]
-# print(student) #list are mutable
-# student[0] = "Surajyadav"
-# print(student)
-# #LIST SLICING
-# marks = [87,64,33,95,76]
-# print(marks[1:len(marks)])
+#LIST IN PYTHON (built in data type)
+marks = [34.4,52.4,64.7,99.4]
+print(marks,type(marks))
+#INDEXING
+print(marks[2])
+#LENGTH OF LIST
+print(len(marks))
+#Store elements of different types
+student = ["Suraj",91.2,19,"Rohtak"]
+print(student) #list are mutable
+student[0] = "Surajyadav"
+print(student)
+#LIST SLICING
+marks = [87,64,33,95,76]
+print(marks[1:len(marks)])
 # #LIST METHODS (they have no return any type of value because it change in original string)
-# list = [2,1,3]
-# list.append(4) #adds one element at the end[1,2,3,4]
-# print(list)
-# list.sort() #sorts in ascending order
-# list.sort(reverse= True) #sorts in descending order
-# print(list)
-# list.reverse()#reverses list 
-# print(list)
-# list.insert(2,"Suraj")#insert element at a particular index
-# print(list)
-# list.remove(1)#removes first occurrence of element 
-# print(list)
-# list.pop(1) #removes element at idx
-# print(list)
+list = [2,1,3]
+list.append(4) #adds one element at the end[1,2,3,4]
+print(list)
+list.sort() #sorts in ascending order
+list.sort(reverse= True) #sorts in descending order
+print(list)
+list.reverse()#reverses list 
+print(list)
+list.insert(2,"Suraj")#insert element at a particular index
+print(list)
+list.remove(1)#removes first occurrence of element 
+print(list)
+list.pop(1) #removes element at idx
+print(list)
+
+#TUPLES IN PYTHON (IMMUTABLE)
+#it does not support item assignment
+tup = (87,64,33,95,76)
+print(type(tup))
+print(tup[2])
+tups = (1,)
+tups = ()
+print(tups)
+#SLICING
+print(tup[1:3])
+#TUPLES METHOD
+tup = (2,1,3,1)
+tup = tup.index(1)
+print(tup)
+print(tup.count(1))
+
