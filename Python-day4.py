67446f94c2894304275432f110e9948870bb77f4
surajyaduvanshi07 <surajyaduvanshi0706@gmail.com>
2026-01-23 11:49:43 -0800

    Not done yet

--- Python-day4.py
+++ Python-day4.py
@@ -2,50 +2,50 @@
 # WHILE LOOP (SYNTAX)
 #while (condition):
        #statement
-# while True :
-#     print("hello")
-# count = 1 #iterator
-# while count <= 5:
-#     print("Hello")
-#     count += 1
-# print(count)
-# i = 1
-# while i <= 5:
-#     print(i)
-#     i += 1
+while True :
+    print("hello")
+count = 1 #iterator
+while count <= 5:
+    print("Hello")
+    count += 1
+print(count)
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
 
-# print("Loop Ended")
-# i = 5
-# while i >= 1:
-#     print(i)
-#     i -= 1
-# print("Loop Ended")
+print("Loop Ended")
+i = 5
+while i >= 1:
+    print(i)
+    i -= 1
+print("Loop Ended")
 
 #BREAK AND CONTINUE STATEMENTS
 #BREAK : used to terminate the loop when encountered
-# i = 1
-# while i <= 5:
-#     print(i)
-#     if(i == 3):
-#         break
-#     i += 1
-# print("end of loop")
+i = 1
+while i <= 5:
+    print(i)
+    if(i == 3):
+        break
+    i += 1
+print("end of loop")
 
 #CONTINUE : terminates execution in the current iteration and continues execution of the loop with the next iteration
-# i = 0
-# while i <=5:
-#     if(i == 3):
-#         i += 1
-#         continue #skip
-#     print(i)
-#     i += 1
-# i = 1
-# while i <= 10:
-#     if (i%2 == 0):
-#         i += 1
-#         continue
-#     print("odd number",i)
-#     i += 1
+i = 0
+while i <=5:
+    if(i == 3):
+        i += 1
+        continue #skip
+    print(i)
+    i += 1
+i = 1
+while i <= 10:
+    if (i%2 == 0):
+        i += 1
+        continue
+    print("odd number",i)
+    i += 1
 #FOR LOOP IN PYTHON
 #For loop are used for sequential traversal. for traversing list, string, tuples etc
 nums = [1,2,3,5,6,7]
@@ -53,3 +53,4 @@ for val in nums:
     print(val)
 else:
     print("end")
+#RANGE IN PYTHON
