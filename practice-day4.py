709c202906616169081502b7249b3947fb5aa3de
surajyaduvanshi07 <surajyaduvanshi0706@gmail.com>
2026-01-23 11:06:29 -0800

    While loop

---
+++ practice-day4.py
@@ -0,0 +1,42 @@
+#WHILE LOOP PRACTICE QUESTION
+#WAP to print number 1 to 100
+# i = 1
+# while i <= 100: #stopin condition
+#     print("i is : ",i)
+#     i += 1
+# print("end of loop")
+#WAP to print number 100 to 1
+# i = 100
+# while i >=1:
+#     print("i is : ",i)
+#     i -= 1
+# print("Loop Ended")
+#WAP to print the multiplication table of a number n
+# n = int(input("Enter your number : "))
+# i = n 
+# while i <= n*10:
+#     print(i)
+#     i += n
+#ANOTHER METHOD
+# n = int(input("enter the number: "))
+# i = 1
+# while i <=10:
+#     print(n*i)
+#     i += 1
+# print("end of loop")
+# print("end of a table")
+#WAP ro elements of the following list using a loop
+# num = [1,4,9,16,25,36,49,64,81,100]
+# i = 0
+# while i < len(num):
+#     print(num[i])
+#     i += 1
+# print("Ended loop")
+#WAP to search number 'x' in this tuple using loop
+# tup = (1,4,9,16,25,36,49,64,81,100)
+# x = int(input("enter the value : "))
+# idx = 0
+# while idx < len(tup):
+#      if (tup[idx] == x):
+#           print("found",idx)
+#      idx += 1
