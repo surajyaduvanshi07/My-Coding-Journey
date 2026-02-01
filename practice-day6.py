5987aedb3e2b03b770661f3ac5bbb6b676b1679b
surajyaduvanshi07 <surajyaduvanshi0706@gmail.com>
2026-02-01 10:59:28 -0800

    Done

--- practice-day6.py
+++ practice-day6.py
@@ -4,29 +4,29 @@ we are learning File I/O
 using Java
 i like programming in Java
 """
-# with open("My-Coding-Journey/practice.txt","w") as f:
-#     f.write("Hey everyone\nwe are learning File I/O\nusing Java\ni like programming in Java")
+with open("My-Coding-Journey/practice.txt","w") as f:
+    f.write("Hey everyone\nwe are learning File I/O\nusing Java\ni like programming in Java")
 
-# with open("My-Coding-Journey/practice.txt","r") as f:
-#     data = f.read()
-#     print(data)
+with open("My-Coding-Journey/practice.txt","r") as f:
+    data = f.read()
+    print(data)
 """
 Write a function that replace all occurrences of "Java" with "python" in above file
 """
-# with open("My-Coding-Journey/practice.txt","r") as f:
-#     data  = f.read()
-# new_data = data.replace("Java","Python")
-# print(new_data)
-# with open("My-Coding-Journey/practice.txt","w") as f:
-#     f.write(new_data)
+with open("My-Coding-Journey/practice.txt","r") as f:
+    data  = f.read()
+new_data = data.replace("Java","Python")
+print(new_data)
+with open("My-Coding-Journey/practice.txt","w") as f:
+    f.write(new_data)
 """
 search if word learning exists in the file or not
 """
 word = "learning"
-with open("My-Coding-Journey/practice.txt","r") as f:
-     data  = f.read()
-
-     if(data.find(word) != -1):
+with open ("My-Coding-Journey/practice.txt","r") as f:
+    data = f.read()
+    if (word in data):
         print("Found")
-     else:
-         print("NOt found")
+    else:
+        print("Not Found")
+
